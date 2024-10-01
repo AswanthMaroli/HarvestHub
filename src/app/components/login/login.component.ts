95c2523ed2bf83e8fc1019748db746b727df89c3
AswanthMaroli <aswanthmaroli385@gmail.com>
2024-10-01 19:32:39 +0530

    harvesthub

---
+++ src/app/components/login/login.component.ts
@@ -0,0 +1,52 @@
+import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
+import { loginData } from '../../Models/loginData';
+import { Users } from '../../Models/users';
+import { Router } from '@angular/router';
+import { LoginService } from '../../services/login.service';
+
+
+@Component({
+  selector: 'app-login',
+  standalone: true,
+  imports: [FormsModule,ReactiveFormsModule],
+  templateUrl: './login.component.html',
+  styleUrl: './login.component.css'
+})
+export class LoginComponent {
+
+  password: string = '';
+  email: string = '';
+  loginData = new loginData();
+  users: Users[] = [];
+  loginForm: FormGroup;
+
+  constructor(private formBuilder: FormBuilder, private router: Router,
+     private LoginService: LoginService
+    ) {
+    this.loginForm = this.formBuilder.group({
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', Validators.required]
+    });
+  }
+
+  async login(){
+
+  if (this.loginForm.valid) {
+    this.loginData.Email = this.loginForm.value.email;
+    this.loginData.Password  = this.loginForm.value.password;
+    await this.LoginService.Authenticate(this.loginData).subscribe((data)=>{
+      console.log(data);
+      let resp = new Response();
+      resp=data;
+      if( resp.Saved==true){
+        alert("Login Success!");
+        this.router.navigate(['']);
+      }else{
+        alert("Login Failed!");
+      }
+  })
+}
+}
+
+}
