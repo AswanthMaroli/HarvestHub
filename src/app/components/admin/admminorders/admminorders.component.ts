d74be4cda85aa507bc35cdc87bff0ae21090fd8e
AswanthMaroli <aswanthmaroli385@gmail.com>
2024-10-09 20:50:04 +0530

    update module

--- src/app/components/admin/admminorders/admminorders.component.ts
+++ src/app/components/admin/admminorders/admminorders.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { Order } from '../../orderdetails/model/order';
 import { ProductRegistrationService } from '../../details/services/product-registration.service';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-admminorders',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './admminorders.component.html',
   styleUrl: './admminorders.component.css'
 })
