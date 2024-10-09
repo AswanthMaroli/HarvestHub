51aa6fc53e41dfd348753cebd694649e075ae73a
AswanthMaroli <aswanthmaroli385@gmail.com>
2024-10-09 23:23:22 +0530

    admin

--- src/app/components/admin/allproducts/allproducts.component.ts
+++ src/app/components/admin/allproducts/allproducts.component.ts
@@ -1,12 +1,36 @@
 import { Component } from '@angular/core';
+import { ProductDetail } from '../../../Models/ProductDetail';
+import { Router } from '@angular/router';
+import { ProductService } from '../../home/services/product.service';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-allproducts',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './allproducts.component.html',
   styleUrl: './allproducts.component.css'
 })
 export class AllproductsComponent {
+  ProductDetails : ProductDetail[] = [];
+  constructor(
+    private ProductService : ProductService,
+              private router : Router,
+              ){}
 
+  async ngOnInit(){  
+   
+    await this.GetProducts();
+  }
+
+  async GetProducts(){
+    await this.ProductService.GetProducts().subscribe((data)=>{
+      console.log('pdata',data);
+      this.ProductDetails=data;
+     if( !this.ProductDetails ){
+      this.ProductDetails=[];
+     }
+    })
+   
+}
 }
